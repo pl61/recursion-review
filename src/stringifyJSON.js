3075aac37809c159f4c9f3d7428003f21f74d6f3
peter <peterliu1991@gmail.com>
2019-01-02 12:28:05 -0800

    completed stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,24 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+    var result = [];
+    for (var i = 0; i < obj.length; i++) {
+      result.push(stringifyJSON(obj[i]));
+    }
+    return '[' + result.join(',') + ']';
+  } else if (typeof obj === 'object' && obj !== null) {
+    var result = [];
+    for (var key in obj) {
+      if (!(typeof obj[key] === 'function' || obj[key] === undefined)) {
+        var current = '';
+        current += '"' + key + '":' + stringifyJSON(obj[key]);
+        result.push(current);
+      }
+    }
+    return '{' + result.join(',') + '}';
+  }
+  return '' + obj;
 };
